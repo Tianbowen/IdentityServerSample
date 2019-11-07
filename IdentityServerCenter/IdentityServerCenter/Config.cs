6f739a73f90168613d1f3abd9f57eeb394a146c5
arwen <1471758682@qq.com>
2019-11-07 16:29:49 +0800

    IdentityServer4简单示例

---
+++ IdentityServerCenter/IdentityServerCenter/Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerCenter
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResource()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("api","My IdentityServer Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClient()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId="client",
+                    RequireClientSecret=true,
+                  AllowedGrantTypes=GrantTypes.ClientCredentials,
+                  ClientSecrets={
+                    new Secret ( "secret".Sha256())
+                    },
+                  AllowedScopes={ "api"}
+                }
+            };
+        }
+
+    }
+}
